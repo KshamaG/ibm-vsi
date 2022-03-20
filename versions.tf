2b368d297ec6ecc28181a8a54e7f18c7da129688
kgurudut <kgurudut@in.ibm.com>
2022-03-20 19:53:47 +0530

    upgraded files

--- versions.tf
+++ versions.tf
@@ -1,4 +1,18 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    ibm = {
+      # TF-UPGRADE-TODO
+      #
+      # No source detected for this provider. You must add a source address
+      # in the following format:
+      #
+      source = "IBM-Cloud/ibm"
+      #
+      # For more information, see the provider source documentation:
+      #
+      # https://www.terraform.io/docs/configuration/providers.html#provider-source
+    }
+  }
 }
